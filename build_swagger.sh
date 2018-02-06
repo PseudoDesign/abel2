1c09450e181cc730c62c646441f6b1c36d4cc0d0
vagrant <vagrant@vagrant.vm>
2018-02-06 08:56:18 +0000

    trying to compile swagger codegen

---
+++ build_swagger.sh
@@ -0,0 +1,1 @@
+java -jar swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i https://esi.tech.ccp.is/legacy/swagger.json?datasource=tranquility -l python -o /share/esi_client
