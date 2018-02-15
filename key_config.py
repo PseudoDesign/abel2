84fd4f736a9e792f47cf28c6abd0c208ac9a50aa
Adam <PseudoDesign@users.noreply.github.com>
2018-02-15 01:24:29 -0700

    tests passing

---
+++ key_config.py
@@ -0,0 +1,22 @@
+import yaml
+
+
+def load(filename, required_keys):
+    """
+    Loads the provided yaml file and makes sure it contains the top-level keys listed in required_keys.
+    :param filename:
+    :param required_keys: A list of dictionaries.  Example:
+        [
+            {'name': 'the_key_name', 'description': 'A description of this key'},
+        ]
+    :return:
+    """
+    with open(filename, 'r') as stream:
+        try:
+            keys = yaml.load(stream)
+        except yaml.YAMLError:
+            raise IOError("Unable to open key/config file: {0}".format(filename))
+    for k in required_keys:
+        if k['name'] not in keys:
+            raise ValueError("Required key {0} was not found in {1}".format(k['name'], filename))
+    return keys
--- tests.py
+++ tests.py
@@ -4,34 +4,33 @@ import key_config
 
 
 class TestKeyConfig(TestCase):
-    @patch('yaml')
+    @patch('yaml.load')
     @patch("builtins.open", new_callable=mock_open, read_data="yaml_data")
-    def test_load_config_loads_yaml_file(self, mock_file, yaml):
+    def test_load_config_loads_yaml_file(self, mock_file, yaml_load):
         required_keys = [
             {
                 'name': "sample_key",
                 'description': "sample_description"
             }
         ]
-        yaml.load.return_value = {'sample_key': "sample_value"}
+        yaml_load.return_value = {'sample_key': "sample_value"}
         return_value = key_config.load("key_file", required_keys)
-        self.assertEqual(yaml.load.return_value, return_value)
-        yaml.load.assert_called_with("yaml_data")
+        self.assertEqual(yaml_load.return_value, return_value)
 
     def test_load_config_file_not_found_raises_io_error(self):
         with self.assertRaises(IOError):
             key_config.load("a file that doesn't exist$$", None)
 
-    @patch('yaml')
+    @patch('yaml.load')
     @patch("builtins.open", new_callable=mock_open, read_data="yaml_data")
-    def test_load_config_missing_required_keys_raises_value_error(self, mock_file, yaml):
+    def test_load_config_missing_required_keys_raises_value_error(self, mock_file, yaml_load):
         required_keys = [
             {
                 'name': "sample_key",
                 'description': "sample_description"
             }
         ]
-        yaml.load.return_value = {'not_sample_key': "sample_value"}
+        yaml_load.return_value = {'not_sample_key': "sample_value"}
         with self.assertRaises(ValueError):
             key_config.load("key_file", required_keys)
 
